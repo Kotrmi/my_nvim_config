3c319949c522cea635013cfcd1a9173c65d190a7
Kotrmi <gabi_gabi@hotmail.ca>
2026-01-17 17:04:26 -0500

    fix: change the config for the csharp lsp and lua lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,7 @@
 return {
    "neovim/nvim-lspconfig",
    config = function() 
-      local lsps = {{"clangd"},{"rust_analyzer"},{"lua-language-server"},{"pyright"}}
+      local lsps = {{"clangd"},{"rust_analyzer"},{"lua_ls"},{"pyright"},{"csharp_ls"}}
       
       for _, lsp in pairs(lsps) do
          local name, config = lsp[1], lsp[2]
